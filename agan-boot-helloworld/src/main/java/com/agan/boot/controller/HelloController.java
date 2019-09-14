b51a159ee2e101e3de23a4efb3b1d0a1af2d5997
agan-java <47729842+agan-java@users.noreply.github.com>
2019-09-14 12:41:39 +0800

    init
    init

---
+++ agan-boot-helloworld/src/main/java/com/agan/boot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.agan.boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "hello agan!";
+    }
+}
